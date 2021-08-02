b992944358fc9ecd026f9de809a1ee3d857fc2f6
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-08-02 14:03:12 +0300

    init: add first build of a simple project

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import "../index.html";
+import "../style.css";
+import "../scss/main.scss";
+// import bg from "../assets/logo.jpg";
+
+// document.getElementByClassName(
+//   "background-image"
+// ).style.backgroundImage = `url(${bg})`;
